207910f6afcac418100b590d52a1697d3e729a79
MarceauKa <marceau@casals.fr>
2016-03-28 18:42:42 +0200

    Premier jet d'un module Laravel 5

---
+++ src/Laravel/Http/Middleware/CheckForSpammers.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Akibatech\Laravel\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Arr;
+use Pdp\Parser;
+use Pdp\PublicSuffixList;
+
+/**
+ * This middleware protects your app from spammers based on a dictionary.
+ *
+ * Class CheckForSpammers
+ * @package Akibatech\Laravel\Http\Middleware
+ */
+class CheckForSpammers
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $referer = $request->server->get('referer');
+
+        // Referer is not provided, we continue
+        if (is_null($referer)) {
+            return $next($request);
+        }
+
+        // Handle the dictionnary.
+        // @todo Refactor that
+        $dir  = realpath( dirname(__FILE__) . '/../../../../' ) . '/';
+        $path = $dir . 'spammers.json';
+        $file = file_get_contents($path);
+
+        // Dictionnary is not readable, abort.
+        if (empty($file)) {
+            abort(500, 'Unable to read spammers.json');
+        }
+
+        $dictionary = json_decode($file);
+
+        // Parse the referer
+        $url  = new Parser(new PublicSuffixList([]));
+        $host = $url->parseHost($referer)->host;
+
+        // Compare dictionary against the referer...
+        $search = Arr::where($dictionary, function($key, $value) use ($host) {
+            return mb_stripos($host, $value) !== false;
+        });
+
+        // ...and check for results
+        if (count($search) > 0) {
+            abort(500, 'Spammers protection');
+        }
+
+        // No spam, we can continue :)
+        return $next($request);
+    }
+}
